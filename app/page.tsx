6eb2550c7570a90ee5f8143a0210f13d0af4462d
Sunil Sahoo <sunilsahoo@sunilss681.local>
2024-02-23 14:51:26 +0530

    ui changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 export default function Page() {
   return (
